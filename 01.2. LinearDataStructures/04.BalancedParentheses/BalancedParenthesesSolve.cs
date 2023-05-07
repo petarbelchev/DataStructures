ceaf31267ee95ae7447c8c51111f290d474aa9f3
Petar Belchev <petar9004@gmail.com>
2023-05-07 19:46:31 +0300

    Added Balanced Parentheses solution

---
+++ 01.2. LinearDataStructures/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem04.BalancedParentheses
+{
+	using System.Collections.Generic;
+
+	public class BalancedParenthesesSolve : ISolvable
+	{
+		public bool AreBalanced(string parentheses)
+		{
+			if (parentheses.Length % 2 != 0)
+				return false;
+
+			var stack = new Stack<char>();
+
+			foreach (char p in parentheses)
+			{
+				char lookFor = default;
+
+				if (p == ']') lookFor = '[';
+				else if (p == ')') lookFor = '(';
+				else if (p == '}') lookFor = '{';
+				else stack.Push(p);
+
+				if (lookFor == default)
+					continue;
+
+				if (stack.Pop() != lookFor)
+					return false;
+			}
+
+			return true;
+		}
+	}
+}
