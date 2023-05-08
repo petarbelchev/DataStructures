7d13a028aada64adf0a705e618fc744aa0eded01
Petar Belchev <petar9004@gmail.com>
2023-05-08 19:59:00 +0300

    Added a Sequence N to M task solution

---
+++ 01.2. LinearDataStructures/SequenceNToM.Tests/SequenceNToMTests.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+
+namespace SequenceNToM.Tests
+{
+	public class SequenceNToMTests
+	{
+		private SequenceNToM instance;
+
+		[SetUp]
+		public void Setup() 
+			=> instance = new SequenceNToM();
+
+		[Test]
+		public void Test1()
+		{
+			// Arrange
+			int n = 3;
+			int m = 10;
+			string expected = "3 -> 5 -> 10";
+
+			// Act
+			string actual = instance.FindSequence(n, m);
+
+			// Assert
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void Test2()
+		{
+			// Arrange
+			int n = 5;
+			int m = -5;
+			string expected = "(no solution)";
+
+			// Act
+			string actual = instance.FindSequence(n, m);
+
+			// Assert
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void Test3()
+		{
+			// Arrange
+			int n = 10;
+			int m = 30;
+			string expected = "10 -> 11 -> 13 -> 15 -> 30";
+
+			// Act
+			string actual = instance.FindSequence(n, m);
+
+			// Assert
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+	}
+}
