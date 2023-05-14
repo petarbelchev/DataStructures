2f5384c847c4640f8af9283dc10c56b7f947c03d
Petar Belchev <petar9004@gmail.com>
2023-05-14 18:22:45 +0300

    Refactoring of implementation

--- 02. Trees-Representation-and-Traversal-(BFS-DFS)/Tree/Tree.cs
+++ 02. Trees-Representation-and-Traversal-(BFS-DFS)/Tree/Tree.cs
@@ -32,34 +32,20 @@
 			{
 				this.children.Add(child);
 				child.parent = this;
+
 				return;
 			}
 
 			if (!this.children.Any())
 				throw new ArgumentNullException();
 
-			Queue<Tree<T>> queue = new Queue<Tree<T>>();
-			queue.Enqueue(this);
+			Tree<T> parentNode = FindNodeByBfs(parentKey);
 
-			while (queue.Count > 0)
-			{
-				Tree<T> tree = queue.Dequeue();
-				List<Tree<T>> currChildren = tree.children;
+			if (parentNode == null)
+				throw new ArgumentNullException();
 
-				foreach (var currChild in currChildren)
-				{
-					if (currChild.value.Equals(parentKey))
-					{
-						currChild.children.Add(child);
-						child.parent = currChild;
-						return;
-					}
-
-					queue.Enqueue(currChild);
-				}
-			}
-
-			throw new ArgumentNullException();
+			parentNode.children.Add(child);
+			child.parent = parentNode;
 		}
 
 		public IEnumerable<T> OrderBfs()
@@ -87,81 +73,20 @@
 
 			return result;
 		}
-
-		private void Dfs(Tree<T> tree, ICollection<T> result)
-		{
-			foreach (var child in tree.children)
-				Dfs(child, result);
-
-			result.Add(tree.value);
-		}
-
-		public void RemoveNode(T nodeKey)
-		{
-			//RemoveByBfs(nodeKey);
-			RemoveByDfs(nodeKey);
-		}
-
-		private void RemoveByBfs(T nodeKey)
-		{
-			if (this.value.Equals(nodeKey))
-				throw new ArgumentException();
-
-			Queue<Tree<T>> queue = new Queue<Tree<T>>();
-
-			foreach (Tree<T> child in this.children)
-				queue.Enqueue(child);
-
-			while (queue.Count > 0)
-			{
-				Tree<T> node = queue.Dequeue();
-
-				if (node.value.Equals(nodeKey))
-				{
-					Tree<T> parent = node.parent;
-					parent.children.Remove(node);
-					node.parent = null;
-
-					return;
-				}
-
-				foreach (Tree<T> child in node.children)
-					queue.Enqueue(child);
-			}
-
-			throw new ArgumentNullException();
-		}
-
-		private void RemoveByDfs(T nodeKey)
-		{
-			if (this.value.Equals(nodeKey))
-				throw new ArgumentException();
-
-			Tree<T> node = FindNodeByDfs(this, nodeKey);
-
-			if (node != null)
-			{
-				Tree<T> parent = node.parent;
-				parent.children.Remove(node);
-				node.parent = null;
-			}
-			else
-			{
-				throw new ArgumentNullException();
-			}
-		}
-
+		
 		public void Swap(T firstKey, T secondKey)
 		{
 			if (this.value.Equals(firstKey) || this.value.Equals(secondKey))
 				throw new ArgumentException();
 
-			Tree<T> firstNode = FindNodeByDfs(this, firstKey);
+			//Tree<T> firstNode = FindNodeByDfs(this, firstKey);
+			Tree<T> firstNode = FindNodeByBfs(firstKey);
 
 			if (firstNode == null)
 				throw new ArgumentNullException();
 
-			Tree<T> secondNode = FindNodeByDfs(this, secondKey);
+			//Tree<T> secondNode = FindNodeByDfs(this, secondKey);
+			Tree<T> secondNode = FindNodeByBfs(secondKey);
 
 			if (secondNode == null)
 				throw new ArgumentNullException();
@@ -187,6 +112,73 @@
 				secondNodeParent.children[secondNodeIndex] = firstNode;
 				firstNode.parent = secondNodeParent;
 			}
+		}
+		
+		public void RemoveNode(T nodeKey)
+		{
+			//RemoveByBfs(nodeKey);
+			RemoveByDfs(nodeKey);
+		}
+
+		private void RemoveByBfs(T nodeKey)
+		{
+			if (this.value.Equals(nodeKey))
+				throw new ArgumentException();
+
+			Tree<T> node = FindNodeByBfs(nodeKey);
+
+			if (node == null)
+				throw new ArgumentNullException();
+
+			Tree<T> parentNode = node.parent;
+			parentNode.children.Remove(node);
+			node.parent = null;
+		}
+
+		private void RemoveByDfs(T nodeKey)
+		{
+			if (this.value.Equals(nodeKey))
+				throw new ArgumentException();
+
+			Tree<T> node = FindNodeByDfs(this, nodeKey);
+
+			if (node != null)
+			{
+				Tree<T> parent = node.parent;
+				parent.children.Remove(node);
+				node.parent = null;
+			}
+			else
+				throw new ArgumentNullException();
+		}
+		
+		private void Dfs(Tree<T> tree, ICollection<T> result)
+		{
+			foreach (var child in tree.children)
+				Dfs(child, result);
+
+			result.Add(tree.value);
+		}
+
+		private Tree<T> FindNodeByBfs(T parentKey)
+		{
+			Queue<Tree<T>> queue = new Queue<Tree<T>>();
+			queue.Enqueue(this);
+
+			while (queue.Count > 0)
+			{
+				Tree<T> subtree = queue.Dequeue();
+
+				foreach (var child in subtree.children)
+				{
+					if (child.value.Equals(parentKey))
+						return child;
+
+					queue.Enqueue(child);
+				}
+			}
+
+			return null;
 		}
 
 		private Tree<T> FindNodeByDfs(Tree<T> root, T nodeKey)
