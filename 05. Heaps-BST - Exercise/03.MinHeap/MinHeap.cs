df62158c5698f7b8535d6573e77099a7dd829e06
Petar Belchev <petar9004@gmail.com>
2023-05-21 14:13:32 +0300

    Implemented Min Heap and Priority Queue

---
+++ 05. Heaps-BST - Exercise/03.MinHeap/MinHeap.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.MinHeap
+{
+	public class MinHeap<T> : IAbstractHeap<T>
+		where T : IComparable<T>
+	{
+		protected List<T> elements;
+
+		public MinHeap() => elements = new List<T>();
+
+		public int Count => elements.Count;
+
+		public void Add(T element)
+		{
+			elements.Add(element);
+			HeapifyUp(Count - 1);
+		}
+
+		private void HeapifyUp(int index)
+		{
+			int parentIndex = (index - 1) / 2;
+
+			if (IsGreater(parentIndex, index))
+			{
+				Swap(index, parentIndex);
+				HeapifyUp(parentIndex);
+			}
+		}
+
+		private void Swap(int index, int swapWithIndex)
+		{
+			T element = elements[index];
+			elements[index] = elements[swapWithIndex];
+			elements[swapWithIndex] = element;
+		}
+
+		public T ExtractMin()
+		{
+			ValidateIsNotEmpty();
+			T element = elements[0];
+			elements[0] = elements[Count - 1];
+			elements.RemoveAt(Count - 1);
+			HeapifyDown(0);
+
+			return element;
+		}
+
+		private void HeapifyDown(int index)
+		{
+			int leftIndex = 2 * index + 1;
+			int rightIndex = 2 * index + 2;
+			int smallerIndex = GetSmallerIndex(leftIndex, rightIndex);
+
+			if (IsValidIndex(smallerIndex) && IsGreater(index, smallerIndex))
+			{
+				Swap(index, smallerIndex);
+				HeapifyDown(smallerIndex);
+			}
+		}
+
+		protected int GetSmallerIndex(int left, int right)
+		{
+			if (IsValidIndex(right) && IsGreater(left, right))
+				return right;
+			else if (IsValidIndex(left))
+				return left;
+			else
+				return -1;
+		}
+
+		protected bool IsGreater(int index, int compareWithIndex)
+			=> elements[index].CompareTo(elements[compareWithIndex]) > 0;
+
+		protected bool IsValidIndex(int index)
+			=> index >= 0 && index < Count;
+
+		public T Peek()
+		{
+			ValidateIsNotEmpty();
+
+			return elements[0];
+		}
+
+		protected void ValidateIsNotEmpty()
+		{
+			if (elements.Count == 0)
+				throw new InvalidOperationException();
+		}
+	}
+}
---
+++ 05. Heaps-BST - Exercise/03.MinHeap/PriorityQueue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.MinHeap
+{
+	public class PriorityQueue<T> : MinHeap<T> where T : IComparable<T>
+	{
+		private Dictionary<T, int> indexesByKeys;
+
+		public PriorityQueue()
+		{
+			elements = new List<T>();
+			indexesByKeys = new Dictionary<T, int>();
+		}
+
+		public void Enqueue(T element)
+		{
+			elements.Add(element);
+			indexesByKeys.Add(element, Count - 1);
+			HeapifyUp(Count - 1);
+		}
+
+		private void HeapifyUp(int index)
+		{
+			int parentIndex = (index - 1) / 2;
+
+			if (IsValidIndex(parentIndex) && IsGreater(parentIndex, index))
+			{
+				Swap(index, parentIndex);
+				HeapifyUp(parentIndex);
+			}
+		}
+
+		private void Swap(int index, int swapWithIndex)
+		{
+			T element = elements[index];
+			elements[index] = elements[swapWithIndex];
+			elements[swapWithIndex] = element;
+
+			indexesByKeys[element] = swapWithIndex;
+			indexesByKeys[elements[index]] = index;
+		}
+
+		public T Dequeue()
+		{
+			ValidateIsNotEmpty();
+			T element = elements[0];
+			elements[0] = elements[Count - 1];
+			elements.RemoveAt(Count - 1);
+			HeapifyDown(0);
+
+			return element;
+		}
+
+		private void HeapifyDown(int index)
+		{
+			int left = 2 * index + 1;
+			int right = 2 * index + 2;
+			int smallerIndex = GetSmallerIndex(left, right);
+
+			if (IsValidIndex(smallerIndex) && IsGreater(index, smallerIndex))
+			{
+				Swap(index, smallerIndex);
+				HeapifyDown(index);
+			}
+		}
+
+		public void DecreaseKey(T key)
+			=> HeapifyUp(indexesByKeys[key]);
+	}
+}
