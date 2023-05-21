57f8d182294a5793994ffecaa99696fa09780340
Petar Belchev <petar9004@gmail.com>
2023-05-21 14:50:41 +0300

    Added my implementation of Cookie Problem task

---
+++ 05. Heaps-BST - Exercise/04.CookiesProblem/CookiesProblem.cs
@@ -0,0 +1,31 @@
+﻿using _03.MinHeap;
+
+namespace _04.CookiesProblem
+{
+	public class CookiesProblem
+    {
+        public int Solve(int minSweetness, int[] cookies)
+        {
+            MinHeap<int> minHeap = new MinHeap<int>();
+            
+            foreach (var cookie in cookies)
+                minHeap.Add(cookie);
+            
+            int counter = 0;
+            
+            while (minHeap.Peek() < minSweetness)
+            {
+                if (minHeap.Count == 1)
+                    return -1;
+             
+                int least = minHeap.ExtractMin();
+                int secondLeast = minHeap.ExtractMin();
+                int newCookie = least + 2 * secondLeast;
+                minHeap.Add(newCookie);
+                counter++;
+            }
+            
+            return counter;
+        }
+    }
+}
